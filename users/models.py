b175a82c3fb30b8967853bf10a82ecc9038b6907
Alexandra <alexandrasolovjova@mail.ru>
2023-10-08 21:02:09 +0300

    create course and lesson models

--- users/models.py
+++ users/models.py
@@ -14,3 +14,30 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    preview = models.ImageField(verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(verbose_name='превью', **NULLABLE)
+    video_url = models.URLField(verbose_name='ссылка')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
