bc6f50fbc734b193b3174cc3e5916194146448d8
Alexandra <alexandrasolovjova@mail.ru>
2023-10-09 18:54:59 +0300

    add payment view

---
+++ courses/views/payment.py
@@ -0,0 +1,13 @@
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.generics import ListAPIView
+
+from courses.models import Payment
+from courses.serializers.payment import PaymentSerializer
+
+
+class PaymentListView(ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [OrderingFilter, SearchFilter]
+    search_fields = ['course', 'lesson', 'payment_type']
+    ordering_fields = ['date_payment', '-date_payment']
