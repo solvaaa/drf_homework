870ec7241146712c6af7a819d404ba5e1830989c
Alexandra <alexandrasolovjova@mail.ru>
2023-10-09 17:21:11 +0300

    create adddummypayments command

---
+++ courses/management/commands/adddummypayments.py
@@ -0,0 +1,28 @@
+from django.core.management import BaseCommand
+
+from courses.models import Lesson, Course, Payment
+from users.models import User
+from random import sample, randint, choice
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        users = list(User.objects.all())
+        lessons = list(Lesson.objects.all())
+        courses = list(Course.objects.all())
+        courses.extend(lessons)
+        num_of_users_paid = randint(min(4, len(users)), len(users))
+        users_paid = sample(users, num_of_users_paid)
+        for user in users_paid:
+            paid_for = choice(courses)
+            course_paid = paid_for if isinstance(paid_for, Course) else None
+            lesson_paid = paid_for if isinstance(paid_for, Lesson) else None
+            payment = Payment.objects.create(
+                user=user,
+                course=course_paid,
+                lesson=lesson_paid,
+                total=randint(100, 100000),
+                payment_type=choice([Payment.CASH, Payment.CREDIT])
+            )
+            payment.save()
