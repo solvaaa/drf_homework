4bc8f6655111e2c06a138f17e09375eaea6685a0
Alexandra <alexandrasolovjova@mail.ru>
2023-10-09 20:03:59 +0300

    add addmoderator command

---
+++ users/management/commands/addmoderators.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for i in range(3):
+            user = User.objects.create(
+                email=f'moderator{i}@mail.ru',
+                first_name=f'Moderator{i}',
+                last_name='Project',
+                is_staff=True,
+                is_superuser=False
+            )
+
+            user.set_password('11qwerty11')
+            user.save()
