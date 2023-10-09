52bb66c8d6d6a1d69bc171783ecdfb971800f926
Alexandra <alexandrasolovjova@mail.ru>
2023-10-09 16:38:23 +0300

    add lesson_count to course serializer

--- courses/serializers/course.py
+++ courses/serializers/course.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from courses.models import Course
+from courses.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_lesson_count(self, instance):
+        return Lesson.objects.filter(course=instance).count()
