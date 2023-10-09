72ea7d7853957541e156d9e7e254ba3d47fdf883
Alexandra <alexandrasolovjova@mail.ru>
2023-10-09 17:21:26 +0300

    register Payment in admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from courses.models import Course, Lesson
+from courses.models import Course, Lesson, Payment
 
 # Register your models here.
 
 
 admin.site.register(Course)
 admin.site.register(Lesson)
+admin.site.register(Payment)
