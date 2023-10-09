21bb87c3af5ff7b984500d6556bd2ef1579051b8
Alexandra <alexandrasolovjova@mail.ru>
2023-10-09 16:11:15 +0300

    remove unused imports

--- courses/views/course.py
+++ courses/views/course.py
@@ -1,6 +1,3 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from courses.models import Course
