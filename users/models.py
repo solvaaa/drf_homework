26183efb133e90d9d8aeba1b6beb9c358f987799
Alexandra <alexandrasolovjova@mail.ru>
2023-10-15 22:31:04 +0300

    Creae role field in user

--- users/models.py
+++ users/models.py
@@ -5,12 +5,18 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
+class UserRoles(models.TextChoices):
+    MEMBER = ('member', 'пользователь')
+    MODERATOR = ('moderator', 'модератор')
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
     phone = models.CharField(max_length=30, verbose_name='телефон', **NULLABLE)
     city = models.CharField(max_length=30, verbose_name='город', **NULLABLE)
     avatar = models.ImageField(verbose_name='аватар', **NULLABLE)
+    role = models.CharField(max_length=12, choices=UserRoles.choices, default=UserRoles.MEMBER)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
