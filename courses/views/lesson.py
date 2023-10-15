2d7940b22af1bc17a0fe6178327dfb91f4292afc
Alexandra <alexandrasolovjova@mail.ru>
2023-10-15 23:45:36 +0300

    list view only for owned lessons

--- courses/views/lesson.py
+++ courses/views/lesson.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Lesson
 from courses.serializers.lesson import LessonSerializer
+from users.models import UserRoles
 from users.permissions import IsOwner, IsModerator, IsSuperUser
 
 
@@ -13,9 +14,15 @@ class LessonDetailView(RetrieveAPIView):
 
 
 class LessonListView(ListAPIView):
-    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == UserRoles.MODERATOR:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonCreateView(CreateAPIView):
