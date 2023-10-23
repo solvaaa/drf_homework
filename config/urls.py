fa36d5e0e893ceca39bce03e017d800681380896
Alexandra <alexandrasolovjova@mail.ru>
2023-10-23 22:43:59 +0300

    Modify documentation

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="My API Docs",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
