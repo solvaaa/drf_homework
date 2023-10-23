0ed34e5fdb35c15f68c15febe8b24139a8b8fa88
Alexandra <alexandrasolovjova@mail.ru>
2023-10-23 21:29:07 +0300

    add payment api integration

--- courses/views/payment.py
+++ courses/views/payment.py
@@ -1,9 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from courses.models import Payment
+from courses.models import Payment, Course
 from courses.serializers.payment import PaymentSerializer
+import courses.payment as pay
 
 
 class PaymentListView(ListAPIView):
@@ -12,3 +15,15 @@ class PaymentListView(ListAPIView):
     filter_backends = [OrderingFilter, DjangoFilterBackend]
     filterset_fields = ['course', 'lesson', 'payment_type']
     ordering_fields = ['date_payment', '-date_payment']
+
+
+class PaymentPayView(APIView):
+
+    def post(self, request, course_id=None):
+        course = Course.objects.get(id=course_id)
+        product = pay.get_stripe_product(course)
+        price = pay.get_stripe_price(course)
+        response = pay.create_session(price)
+
+        return Response({"url": response["url"]})
+
